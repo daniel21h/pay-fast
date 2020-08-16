08e44be73c9e513c80133c0cff6e61fc02ee8160
daniel21h <hesseldaniel21@gmail.com>
2020-08-15 21:41:35 -0300

    feat: add payment route

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = () => {
   const server = express();
+
+  // Body parser
+  server.use(bodyParser.urlencoded({ extended: true }));
+  server.use(bodyParser.json());
 
   // Files manager
   consign()
--- src/controllers/Payments.js
+++ src/controllers/Payments.js
@@ -1,5 +1,12 @@
 module.exports = (server) => {
   server.get('/payments', (request, response) => {
     response.json({ message: 'Alura Node.js' })
-  })
+  });
+
+  server.post('/payments/payment', (request, response) => {
+    const payment = request.body;
+    console.log(payment);
+
+    response.send('Ok');
+  });
 }
